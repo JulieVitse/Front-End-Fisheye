4f9c5ed17b39746ef5f8d4a5fa125fdb0063dacf
Julie V <julievitse91@gmail.com>
2022-05-25 08:27:21 +0200

    fetched json data & consctructed base index page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,38 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+
+        const img = document.createElement('img');
+        const h2 = document.createElement('h2');
+        const articleLink = document.createElement('a');
+        const h3 = document.createElement('h3');
+        const pTagline = document.createElement('p');
+        const pPrice = document.createElement('p');
+
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        articleLink.setAttribute("href", '#');
+        articleLink.appendChild(img);
+        articleLink.appendChild(h2);
+        
+        h3.textContent = city + ", " + country;
+        
+        pTagline.textContent = tagline;
+        
+        pPrice.textContent = price + '€/jour';
+        pPrice.className = 'price';
+
+        article.appendChild(articleLink);
+        article.appendChild(h3);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, id, getUserCardDOM }
 }
